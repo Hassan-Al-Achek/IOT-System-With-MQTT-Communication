45c8b87a3772534ada0250d55d4262a903bf37d2
= <=>
2021-12-28 13:26:34 +0100

    first commit

---
+++ ESP32_BLE_Server.ino
@@ -0,0 +1,67 @@
+#include <BLEDevice.h>
+#include <BLEUtils.h>
+#include <BLEServer.h>
+
+
+#define SERVICE_UUID "4fafc201-1fb5-459e-8fcc-c5c9c331914b"
+#define CHARACTERISTIC_UUID "beb5483e-36e1-4688-b7f5-ea07361b26a8"
+
+class MyCallbacks: public BLECharacteristicCallbacks {
+    void onWrite(BLECharacteristic *pCharacteristic) {
+      if(pCharacteristic->getValue() == "Hello World says Neil"){
+      pCharacteristic->setValue("Hassan");
+      std::string value = pCharacteristic->getValue();
+         
+
+      }
+       digitalWrite(13, HIGH);
+ }
+ void onRead(BLECharacteristic *pCharacteristic){
+    std::string value = pCharacteristic->getValue();
+          digitalWrite(13, HIGH);
+    Serial.println(pCharacteristic->getValue().c_str());
+  }
+
+};
+
+void setup()
+{
+    Serial.begin(115200);
+    Serial.println("Starting BLE work!");
+
+    pinMode(13, OUTPUT);
+    
+    BLEDevice::init("IOTGroup19");
+    BLEServer *pServer = BLEDevice::createServer();
+    
+    /*Declare Service*/
+    BLEService *pService = pServer->createService(SERVICE_UUID);
+
+    /*Declare Characteristic*/
+    BLECharacteristic *pCharacteristic = pService->createCharacteristic(
+        CHARACTERISTIC_UUID,
+            BLECharacteristic::PROPERTY_WRITE | 
+            BLECharacteristic::PROPERTY_READ );
+
+    pCharacteristic->setCallbacks(new MyCallbacks());
+    
+    pCharacteristic->setValue("Hello World says Neil");
+
+    pService->start();
+    
+    BLEAdvertising *pAdvertising = BLEDevice::getAdvertising();
+    pAdvertising->addServiceUUID(SERVICE_UUID);
+    pAdvertising->setScanResponse(true);
+    pAdvertising->setMinPreferred(0x06); // functions that help with iPhone connections issue
+    pAdvertising->setMinPreferred(0x12);
+    
+
+    
+    Serial.println("Characteristic defined! Now you can read it in your phone!");
+}
+
+void loop()
+{
+    BLEDevice::startAdvertising();
+    delay(2000);
+}
